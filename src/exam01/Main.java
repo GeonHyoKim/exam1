fa62e315a86a868f8acc8fbad9c31cd03297a2cd
GeonHyoKim <rlarjsgy69@naver.com>
2024-10-11 17:30:50 +0900

    stars

---
+++ src/exam01/Main.java
@@ -0,0 +1,19 @@
+package exam01;
+
+import java.util.Scanner;
+
+public class Main {
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int x = sc.nextInt();
+		int y = sc.nextInt();
+		
+		for(int i = 1; i<=y; i++) {
+			for (int j = 1; j<=x; j++) {
+				System.out.print("*");
+			}
+			System.out.println("");
+		}
+	}
+}
